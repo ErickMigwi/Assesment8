55525359270945c10272f431b3b51361f3f6ce60
ErickMigwi <muthigaerick@gmail.com>
2023-03-10 11:22:57 +0300

    update files

--- src/components/OnePost.jsx
+++ src/components/OnePost.jsx
@@ -1,6 +1,8 @@
-import React, {useState} from 'react'
+import React, {useState, useContext} from 'react'
+import { PostContext } from './Posts'
 
 function OnePost() {
+    let post = useContext(PostContext)
     const [value, setValue] = useState(0)
     const [id, setId] = useState(0)
     let handleChange = (e)=>{
@@ -10,14 +12,26 @@ function OnePost() {
     let handleClick = ()=>{
     setId(value)
     console.log(id);
+    console.log(post);
     }
+    const filteredPost = post.find(p => p.id === id)
   return (
     <div>
+
         <h1>Enter id:</h1>
         <label> Id:
       <input type="text" onChange={handleChange} />
       <button onClick={handleClick}>submit</button>
       </label>
+      
+      {filteredPost ? (
+          <div>
+            <h2>{filteredPost.title}</h2>
+            <p>{filteredPost.body}</p>
+          </div>
+        ) : (
+          <p>No post found with id: {id}</p>
+        )}
     </div>
   )
 }
--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,37 +1,35 @@
-import React, { useState, createContext,useEffect } from 'react';
+import React, { useState, createContext, useEffect } from 'react';
 import axios from 'axios';
-
-
+export const PostContext = createContext([])
 function Posts() {
   const [posts, setPosts] = useState([]);
+
   async function getPosts() {
     try {
       const response = await axios.get('https://dummyjson.com/posts');
       setPosts(response.data.posts);
-      console.log(posts);
     } catch (error) {
       console.error(error);
     }
   }
+
   useEffect(() => {
     getPosts();
   }, []);
 
   return (
-  
+    <PostContext.Provider value={posts}>
     <div>
-       
       {posts?.map(post => (
-        // console.log(post)
         <div key={post.id}>
           <h2>{post.title}</h2>
           <p>{post.body}</p>
         </div>
       ))}
-       
     </div>
-   
+    </PostContext.Provider>
   );
 }
-// export {posts};
+
 export default Posts;
+// export { posts };
